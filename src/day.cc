237410f02e7359c30ee0d28073431ed725740d7e
Tomas Forsblom-Strand <tomas@fik1.net>
2023-12-02 15:04:19 +0200

    Change ReadFromFile

--- src/day.cc
+++ src/day.cc
@@ -7,9 +7,9 @@ void IDay::ReadFromFile(const std::string &file_name)
 {
     std::ifstream read_file(file_name);
     assert(read_file.is_open());
-
-    std::copy(std::istream_iterator<std::string>(read_file), std::istream_iterator<std::string>(),
-        std::back_inserter(this->lines));
-
+    std::string line;
+    while (std::getline(read_file, line)) {
+        this->lines.push_back(line);
+    }
     read_file.close();
 }
