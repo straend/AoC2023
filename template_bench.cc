f2445e19a4b9bbba38c76718af4dd5d8ce44f472
Tomas Forsblom-Strand <tomas@fik1.net>
2023-12-03 20:03:26 +0200

    Fix bench template

--- template_bench.cc
+++ template_bench.cc
@@ -9,9 +9,9 @@ static void AOC23_DayDAY_NUMBER_part1(benchmark::State& state) {
 BENCHMARK(AOC23_DayDAY_NUMBER_part1);
 
 static void AOC23_DayDAY_NUMBER_part2(benchmark::State& state) {
-  AOC23::Day1 a("../inputs/dayDAY_NUMBER.txt");
+  AOC23::DayDAY_NUMBER a("../inputs/dayDAY_NUMBER.txt");
   for (auto _ : state)
-    a.Part1();
+    a.Part2();
 }
 
 BENCHMARK(AOC23_DayDAY_NUMBER_part2);
