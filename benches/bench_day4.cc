fa1deac1608138e3e3ba2a9fc49aa6fc18163bf6
Tomas Forsblom-Strand <tomas@fik1.net>
2023-12-09 23:39:14 +0200

    Day 4 Bench

--- benches/bench_day4.cc
+++ benches/bench_day4.cc
@@ -1,6 +1,12 @@
 #include <benchmark/benchmark.h>
 #include "../src/day4.h"
 
+static void AOC23_Day4_parse(benchmark::State& state) {
+  for (auto _ : state)
+    AOC23::Day4 a("../inputs/day4.txt");
+}
+BENCHMARK(AOC23_Day4_parse);
+
 static void AOC23_Day4_part1(benchmark::State& state) {
   AOC23::Day4 a("../inputs/day4.txt");
   for (auto _ : state)
